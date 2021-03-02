5d382831911919b2636f4230ce0150f039d08c5b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-03 01:26:11 +0300

    feat: add 'fullscreen-button' component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,5 +1,6 @@
-import { Container, Typography } from '@material-ui/core';
+import { Button, ButtonGroup, Container, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import FullscreenButton from 'Components/FullscreenButton';
 import Gameboard from 'Components/Gameboard';
 import * as React from 'react';
 
@@ -14,10 +15,20 @@ const MainView = (): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <Container className={classes.main} component="main" maxWidth="sm">
+    <Container className={classes.main} component="main" maxWidth="xs">
+      <FullscreenButton />
       <Typography component="h1" gutterBottom variant="h5">
         Reversi game!
       </Typography>
+      <ButtonGroup
+        aria-label="outlined secondary button group"
+        color="primary"
+        variant="contained"
+      >
+        <Button>New Game</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
       <Gameboard />
     </Container>
   );
