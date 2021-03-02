44697dc7a706a7de6ae66c2185ae2018f352737b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-03 02:20:53 +0300

    refactor: remove redundant gameboard-model export from gameboard state
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/redux-states/gameboard/index.ts
+++ src/redux-states/gameboard/index.ts
@@ -1,5 +1,4 @@
 import * as gameboardActions from './actions';
-import * as gameboardModel from './model';
 import gameboardReducer from './reducer';
 
-export { gameboardActions, gameboardReducer, gameboardModel };
+export { gameboardActions, gameboardReducer };
